f9ff64383beb0e2591e10b7286125ad9543fec35
ghadiadeepali <deepalinghadia1606@gmail.com>
2025-06-14 12:34:39 +0530

    added rest_framework to INSTALLED_APPS

--- wizvault/wizvault/settings.py
+++ wizvault/wizvault/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "mindlog"
+    "mindlog",
+    "rest_framework"
 ]
 
 MIDDLEWARE = [
