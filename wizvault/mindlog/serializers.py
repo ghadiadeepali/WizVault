e05741bc93e26d4a984538b3bedea366ad4e7f8a
ghadiadeepali <deepalinghadia1606@gmail.com>
2025-06-16 17:19:40 +0530

    learned about serializers and importance of read_only_fields

--- wizvault/mindlog/serializers.py
+++ wizvault/mindlog/serializers.py
@@ -1,7 +1,16 @@
 from rest_framework import serializers
 from mindlog.models import Card, Category
 
+# for displaying all the fields
 class CardSerializer(serializers.ModelSerializer):
     class Meta:
         model= Card
         fields = "__all__"
+        
+        
+class NewCardSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Card
+        fields = ['id', 'title', 'text_body', 'category', 'created_at', 'updated_at']
+        # all 6 fields will be shown in output but the below 3 will be ignored in input
+        read_only_fields = ['id', 'created_at', 'updated_at']
