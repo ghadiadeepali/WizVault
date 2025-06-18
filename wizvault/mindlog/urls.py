3e2d85910c7f1517ccb5b5b25a44b6d18f70a0d9
ghadiadeepali <deepalinghadia1606@gmail.com>
2025-06-18 15:51:08 +0530

    defined url_pattern for update and delete card

--- wizvault/mindlog/urls.py
+++ wizvault/mindlog/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("api/v1/cards/", views.cardView)
+    path("api/v1/cards/", views.cardView),
+    path("api/v1/card/<int:pk>", views.updateCardView, name="update_card"),
+    path("api/v1/card/<int:pk>/delete", views.deleteCardView)
+    
     ]
+  
