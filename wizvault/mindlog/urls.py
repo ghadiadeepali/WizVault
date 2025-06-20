d784ae2d19a2a51d1c8af8c36c381c856e64e671
ghadiadeepali <deepalinghadia1606@gmail.com>
2025-06-20 17:29:03 +0530

    defined url pattern for update and delete of category

--- wizvault/mindlog/urls.py
+++ wizvault/mindlog/urls.py
@@ -6,7 +6,9 @@ urlpatterns = [
     path("api/v1/cards/", views.list_card_view),
     path("api/v1/card/<int:pk>", views.update_card_view, name="update_card"),
     path("api/v1/card/<int:pk>/delete", views.delete_card_view),
-    path("api/v1/categories/", views.list_category_view)
+    path("api/v1/categories/", views.list_category_view),
+    path("api/v1/category/<int:pk>/", views.update_category_view),
+    path("api/v1/category/delete/<int:pk>/", views.delete_category_view),
     
     ]
   
